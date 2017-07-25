36614919667c7b3e4152c2ba14c6671e4700acaa
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2017-07-25 23:57:07 +0800

    input: misc: Import MPU6050 changes for wt88047
    Change-Id: Id46fb1d1f79f121bc2670e228df31b201085eafe
    Signed-off-by: karthikkm007 <jacksparrow941994@gmail.com>

--- drivers/input/misc/mpu6050.c
+++ drivers/input/misc/mpu6050.c
@@ -39,8 +39,10 @@
 /*VDD 2.375V-3.46V VLOGIC 1.8V +-5%*/
 #define MPU6050_VDD_MIN_UV	2500000
 #define MPU6050_VDD_MAX_UV	3400000
+#ifndef CONFIG_MACH_WT88047
 #define MPU6050_VLOGIC_MIN_UV	1800000
 #define MPU6050_VLOGIC_MAX_UV	1800000
+#endif
 #define MPU6050_VI2C_MIN_UV	1750000
 #define MPU6050_VI2C_MAX_UV	1950000
 
@@ -166,7 +168,9 @@ struct mpu6050_sensor {
 	bool batch_gyro;
 
 	/* power control */
+#ifndef CONFIG_MACH_WT88047
 	struct regulator *vlogic;
+#endif
 	struct regulator *vdd;
 	struct regulator *vi2c;
 	int enable_gpio;
@@ -334,6 +338,7 @@ static int mpu6050_power_ctl(struct mpu6050_sensor *sensor, bool on)
 			return rc;
 		}
 
+#ifndef CONFIG_MACH_WT88047
 		rc = regulator_enable(sensor->vlogic);
 		if (rc) {
 			dev_err(&sensor->client->dev,
@@ -341,6 +346,7 @@ static int mpu6050_power_ctl(struct mpu6050_sensor *sensor, bool on)
 			regulator_disable(sensor->vdd);
 			return rc;
 		}
+#endif
 
 		if (!IS_ERR_OR_NULL(sensor->vi2c)) {
 			rc = regulator_enable(sensor->vi2c);
@@ -348,7 +354,9 @@ static int mpu6050_power_ctl(struct mpu6050_sensor *sensor, bool on)
 				dev_err(&sensor->client->dev,
 					"Regulator vi2c enable failed rc=%d\n",
 					rc);
+#ifndef CONFIG_MACH_WT88047
 				regulator_disable(sensor->vlogic);
+#endif
 				regulator_disable(sensor->vdd);
 				return rc;
 			}
@@ -379,6 +387,7 @@ static int mpu6050_power_ctl(struct mpu6050_sensor *sensor, bool on)
 			return rc;
 		}
 
+#ifndef CONFIG_MACH_WT88047
 		rc = regulator_disable(sensor->vlogic);
 		if (rc) {
 			dev_err(&sensor->client->dev,
@@ -386,6 +395,7 @@ static int mpu6050_power_ctl(struct mpu6050_sensor *sensor, bool on)
 			rc = regulator_enable(sensor->vdd);
 			return rc;
 		}
+#endif
 
 		if (!IS_ERR_OR_NULL(sensor->vi2c)) {
 			rc = regulator_disable(sensor->vi2c);
@@ -431,6 +441,7 @@ static int mpu6050_power_init(struct mpu6050_sensor *sensor)
 		}
 	}
 
+#ifndef CONFIG_MACH_WT88047
 	sensor->vlogic = regulator_get(&sensor->client->dev, "vlogic");
 	if (IS_ERR(sensor->vlogic)) {
 		ret = PTR_ERR(sensor->vlogic);
@@ -449,6 +460,7 @@ static int mpu6050_power_init(struct mpu6050_sensor *sensor)
 			goto reg_vlogic_put;
 		}
 	}
+#endif
 
 	sensor->vi2c = regulator_get(&sensor->client->dev, "vi2c");
 	if (IS_ERR(sensor->vi2c)) {
@@ -471,6 +483,7 @@ static int mpu6050_power_init(struct mpu6050_sensor *sensor)
 
 reg_vi2c_put:
 	regulator_put(sensor->vi2c);
+#ifndef CONFIG_MACH_WT88047
 	if (regulator_count_voltages(sensor->vlogic) > 0)
 		regulator_set_voltage(sensor->vlogic, 0, MPU6050_VLOGIC_MAX_UV);
 reg_vlogic_put:
@@ -478,6 +491,7 @@ reg_vlogic_put:
 reg_vdd_set_vtg:
 	if (regulator_count_voltages(sensor->vdd) > 0)
 		regulator_set_voltage(sensor->vdd, 0, MPU6050_VDD_MAX_UV);
+#endif
 reg_vdd_put:
 	regulator_put(sensor->vdd);
 	return ret;
@@ -487,9 +501,11 @@ static int mpu6050_power_deinit(struct mpu6050_sensor *sensor)
 {
 	int ret = 0;
 
+#ifndef CONFIG_MACH_WT88047
 	if (regulator_count_voltages(sensor->vlogic) > 0)
 		regulator_set_voltage(sensor->vlogic, 0, MPU6050_VLOGIC_MAX_UV);
 	regulator_put(sensor->vlogic);
+#endif
 	if (regulator_count_voltages(sensor->vdd) > 0)
 		regulator_set_voltage(sensor->vdd, 0, MPU6050_VDD_MAX_UV);
 	regulator_put(sensor->vdd);
